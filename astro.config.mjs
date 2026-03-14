6d8c082fa1c51c4765e1ec85062979c1ef926631
obaodelana <obaloluwa.dev@gmail.com>
2026-03-13 23:03:39 -0600

    ci: corrected url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'static',
-  site: "jollofactory.com",
+  output: "static",
+  site: "https://jollofactory.com",
   vite: {
     css: {
-      postcss: './postcss.config.mjs',
+      postcss: "./postcss.config.mjs",
     },
   },
 });
